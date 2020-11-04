bc23a53ec4281a39fd93c38e37b15e1497b52da6
Sultan <sultansailaubaev1998@gmail.com>
2020-11-05 00:13:02 +0600

    Fixed models, migrated, added admin.

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from scraping.models import Country, City, Category, GenderType, AgeInterval, Language, Interval, Account, \
+    AccountCategory, AccountLanguage, AudienceGender, AudienceAge, AudienceCountry, AudienceCity
+
+scraping_models = [Country, City, Category, GenderType, AgeInterval, Language, Interval, Account,
+                   AccountCategory, AccountLanguage, AudienceGender, AudienceAge, AudienceCountry, AudienceCity]
+
+
+admin.site.register(scraping_models)
--- scraping/models.py
+++ scraping/models.py
@@ -1,46 +1,92 @@
 from django.db import models
 
 
-class Interval(models.Model):
-    title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
-
-
-class GenderType(models.Model):
-    title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'gender_types'
-
-
-class AgeInterval(models.Model):
-    title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'age_intervals'
-
-
-class Language(models.Model):
-    title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
-
-
 class Country(models.Model):
     title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return self.title
 
 
 class City(models.Model):
     title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    hype_id = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+
+class GenderType(models.Model):
+    title = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        db_table = 'gender_types'
+        verbose_name = "Gender type"
+        verbose_name_plural = "Gender types"
+
+    def __str__(self):
+        return self.title
+
+
+class AgeInterval(models.Model):
+    title = models.CharField(max_length=255)
+    hype_start_id = models.IntegerField(null=True)
+    hype_end_id = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'age_intervals'
+        verbose_name = "Age interval"
+        verbose_name_plural = "Age intervals"
+
+    def __str__(self):
+        return self.title
+
+
+class Language(models.Model):
+    title = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
+
+    def __str__(self):
+        return self.title
+
+
+class Interval(models.Model):
+    title = models.CharField(max_length=255)
+    hype_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Interval"
+        verbose_name_plural = "Intervals"
+
+    def __str__(self):
+        return self.title
 
 
 class Account(models.Model):
@@ -56,7 +102,7 @@ class Account(models.Model):
     )
 
     name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     is_verified = models.BooleanField(default=False)
     followers = models.IntegerField()
     quality_followers = models.IntegerField()
@@ -66,13 +112,20 @@ class Account(models.Model):
     account_type = models.IntegerField(choices=ACCOUNT_TYPE_SELECT, default=0)
     contact_info = models.IntegerField(choices=CONTACT_INFO_SELECT, default=0)
 
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
     gender = models.ForeignKey(GenderType, blank=True, null=True, on_delete=models.DO_NOTHING, default=None)
     age = models.ForeignKey(AgeInterval, blank=True, null=True, on_delete=models.DO_NOTHING, default=None)
-    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
     city = models.ForeignKey(City, blank=True, null=True, on_delete=models.DO_NOTHING, default=None)
 
     categories = models.ManyToManyField(Category, through='AccountCategory')
     languages = models.ManyToManyField(Language, through='AccountLanguage')
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+
+    def __str__(self):
+        return self.username
 
 
 class AccountCategory(models.Model):
@@ -81,6 +134,11 @@ class AccountCategory(models.Model):
 
     class Meta:
         db_table = 'account_categories'
+        verbose_name = "Account category"
+        verbose_name_plural = "Account categories"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.category)}"
 
 
 class AccountLanguage(models.Model):
@@ -89,6 +147,11 @@ class AccountLanguage(models.Model):
 
     class Meta:
         db_table = 'account_languages'
+        verbose_name = "Account language"
+        verbose_name_plural = "Account languages"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.language)}"
 
 
 class AudienceGender(models.Model):
@@ -98,6 +161,11 @@ class AudienceGender(models.Model):
 
     class Meta:
         db_table = 'audience_gender'
+        verbose_name = "Audience gender"
+        verbose_name_plural = "Audience genders"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.gender_type)}-{str(self.interval)}"
 
 
 class AudienceAge(models.Model):
@@ -107,6 +175,11 @@ class AudienceAge(models.Model):
 
     class Meta:
         db_table = 'audience_age'
+        verbose_name = "Audience age"
+        verbose_name_plural = "Audience ages"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.age_interval)}-{str(self.interval)}"
 
 
 class AudienceCountry(models.Model):
@@ -116,6 +189,11 @@ class AudienceCountry(models.Model):
 
     class Meta:
         db_table = 'audience_country'
+        verbose_name = "Audience country"
+        verbose_name_plural = "Audience countries"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.country)}-{str(self.interval)}"
 
 
 class AudienceCity(models.Model):
@@ -125,3 +203,8 @@ class AudienceCity(models.Model):
 
     class Meta:
         db_table = 'audience_city'
+        verbose_name = "Audience city"
+        verbose_name_plural = "Audience cities"
+
+    def __str__(self):
+        return f"{str(self.account)}-{str(self.city)}-{str(self.interval)}"
